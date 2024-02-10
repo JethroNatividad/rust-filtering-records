3e3bb34b624532d377a03d1b290543b35cc49bf0
JethroNatividad <natividad.jet@gmail.com>
2024-02-10 20:25:46 +0800

    add getinput function

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,23 @@
 // Process: filter in every fields
 // outputs: search results
 
+fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        match input.trim().parse() {
+            Ok(value) => break value,
+            Err(_) => println!("Invalid input. Please try again."),
+        }
+    }
+}
+
 fn main() {
     let dataset: Vec<HashMap<&str, &str>> = vec![
         HashMap::from([
