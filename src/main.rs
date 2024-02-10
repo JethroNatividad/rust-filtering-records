e844bc170902d1d26295046fd74aeacc296b4011
JethroNatividad <natividad.jet@gmail.com>
2024-02-10 19:50:03 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// Create a program that takes in a search string and filters this dataset and print by tabular format.
+// Dataset:
+// First Name Last Name Position Separation date
+// John Johnson Manager 2016-12-31
+// Tou Xiong Software Engineer 2016-10-05
+// Michaela Michaelson District Manager 2015-12-19
+// Jake Jacobson Programmer
+// Jacquelyn Jackson DBA
+// Sally Weber Web Developer 2015-12-18
+
 fn main() {
     println!("Hello, world!");
 }
